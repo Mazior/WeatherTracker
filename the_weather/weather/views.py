be2bf3cdeb6580b5dff607b0c0ba66174a42d853
Mazior <jzioria@gmail.com>
2019-04-11 07:41:20 -0700

    showing weather information in webpage

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,3 +1,21 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=777fa27f36d120c5bdb8679a20318621'    
+    city = 'Abidjan'
+
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    context ={'city_weather': city_weather}
+    print(city_weather)
+    return render(request, 'weather/base.html', context)
+    
