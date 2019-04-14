09dc984e6100fc11bfbe8d1a8bc95aed59ca80ab
Mazior <jzioria@gmail.com>
2019-04-14 03:37:14 -0700

    fixed keyerror and allow user to add city

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,21 +1,39 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
 
 def index(request):
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=777fa27f36d120c5bdb8679a20318621'    
-    city = 'Abidjan'
+    
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    r = requests.get(url.format(city)).json()
+    form = CityForm()
 
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
+    cities = City.objects.all()
 
-    context ={'city_weather': city_weather}
-    print(city_weather)
+    weather_data = []
+    try:
+        for city in cities:    
+            r = requests.get(url.format(city)).json()
+                        
+            city_weather = {
+                'city': city.name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'], 
+            }
+
+            weather_data.append(city_weather)
+    except KeyError:
+        pass
+    except EXCEPTION as e:
+        pass
+
+    context ={'weather_data': weather_data, 'form': form}
     return render(request, 'weather/base.html', context)
     
